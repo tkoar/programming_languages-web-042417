2ef3fe892f3bd0997e9bd9009b8b83cb8e29a0f4
github username <github email address>
2017-04-17 21:11:12 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
 def reformat_languages(languages)
-  # your code here
+  formatted_langs = {}
+  languages.each do |lang_style, langs|
+    langs.each do |language, description|
+      description.each do |attribute, value|
+        if formatted_langs[language].nil?
+          formatted_langs[language] = {}
+        end
+        formatted_langs[language][:style] ||= []
+        formatted_langs[language][:style] << lang_style
+        if formatted_langs[language][attribute].nil?
+          formatted_langs[language][attribute] = value
+        end
+      end
+    end
+  end
+  formatted_langs
 end
